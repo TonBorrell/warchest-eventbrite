e1d0db10076dd7fe4dca9461fce1ddc5c30a5943
tonborrell <tonborrell@gmail.com>
2023-02-26 14:26:12 +0100

    add control and move

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import random
 
+def read_unit():
+    return input('Select unit from your hand: ')
+
+def read_position():
+    pos = input('Select position to place: ')
+    pos = pos.split(',')
+    pos = (pos[0], pos[1])
+    return pos
+
 class Warchest:
     def __init__(self) -> None:
         self.board = Board()
-        self.crow = Player([('C', (4, 2))], {Archer: 3, Berseker: 3})
-        self.wolf = Player([('W', (0, 2))], {Cavalry: 3, Knight: 3})
+        self.crow = Player([ControlZone((4, 2), 'control')], {Archer: 3, Berseker: 3}, {'archer': Archer, 'berseker': Berseker})
+        self.wolf = Player([ControlZone((0, 2), 'control')], {Cavalry: 3, Knight: 3}, {'cavalry': Cavalry, 'knight': Knight})
         self.current_player = self.decide_first_player()
     
     def decide_first_player(self):
@@ -38,10 +47,11 @@ class Board:
         self.pieces = []
 
 class Player:
-    def __init__(self, units, bag) -> None:
+    def __init__(self, units, bag, units_dict) -> None:
         self.units = units # Inside this list --> tuple (Type of unit, Position of unit)
         self.bag = {} # Unit: n_units
         self.bag = bag
+        self.units_dict = units_dict
         self.hand = []
         self.discard_pile = []
         self.control_token = 3
@@ -75,13 +85,47 @@ class Player:
         # Do movement
 
     def place(self):
-        pass
+        unit = read_unit()
+        self.check_if_unit_in_hand(unit)
+        # TODO: Loop until unit in hand
+        pos = read_position()
+        for unit in self.units:
+            if unit.name == 'control':
+                if unit.is_close(pos):
+                    # Set piece in board
+                    unit_class = self.units_dict[unit.lower()(pos)]
+                    self.units.append(unit_class)
+                    self.hand.remove(unit)
+
 
     def control(self):
-        pass
+        # Unit to discard
+        unit = read_unit()
+        self.check_if_unit_in_hand(unit)
+        # TODO: Loop until unit in hand
+        # Position to control
+        pos = read_position()
+        if self.check_if_pos_in_control_zone(pos):
+            # Control
+            self.control_position(pos)
+            # Discard unit
+            self.hand.remove(unit)
 
     def move(self):
-        pass
+        # From position
+        # TODO: Check if unit in this position
+        pos_initial = read_position()
+        # Select piece of same type
+        unit_to_discard = read_unit()
+        self.check_if_unit_in_hand(unit)
+        # TODO: Loop until unit in hand
+        # To position
+        pos_final = read_position()
+        for unit in self.units:
+            if unit.name == unit_to_discard:
+                if unit.isclose(pos_final):
+                    unit.pos = pos_final
+                    self.hand.remove(unit_to_discard)
 
     def recruit(self):
         pass
@@ -93,33 +137,56 @@ class Player:
         pass
 
     def check_if_unit_in_hand(self, unit):
-        pass
+        if unit in self.hand:
+            return True
+        return False
 
-    def check_if_unit_in_control_zone(self, unit):
+    def check_if_pos_in_control_zone(self, pos):
+        # TODO: Check if position is in control zone, control zone is a constant in the game
         pass
 
     def check_if_unit_in_bag(self, unit):
         pass
 
+    def control_position(self, pos):
+        unit_class = self.units_dict[ControlZone(pos)]
+        self.units.append(unit_class)
+        self.control_token -= 1
+
 class Unit:
-    def __init__(self, pos) -> None:
-        pos = self.pos
+    def __init__(self, pos, name) -> None:
+        self.pos = pos
+        self.name = name
+
+    def is_close(self, pos2, max_dif=1):
+        if self.pos[0] + max_dif == pos2[0] or self.pos[0] - max_dif == pos2[0] and self.pos[1] == pos2[1]:
+            return True
+        if self.pos[1] + max_dif == pos2[1] or self.pos[1] - max_dif == pos2[1] and self.pos[0] == pos2[0]:
+            return True
+        return False
+
+    def set_pos(self, pos):
+        self.pos = pos
+
+class ControlZone(Unit):
+    def __init__(self, pos, name) -> None:
+        super().__init__(pos, name)
 
 class Archer(Unit):
-    def __init__(self, pos):
-        super().__init__(pos)
+    def __init__(self, pos, name):
+        super().__init__(pos, name)
 
 class Berseker(Unit):
-    def __init__(self, pos):
-        super().__init__(pos)
+    def __init__(self, pos, name):
+        super().__init__(pos, name)
 
 class Cavalry(Unit):
-    def __init__(self, pos):
-        super().__init__(pos)
+    def __init__(self, pos, name):
+        super().__init__(pos, name)
 
 class Knight(Unit):
-    def __init__(self, pos):
-        super().__init__(pos)
+    def __init__(self, pos, name):
+        super().__init__(pos, name)
 
 
 if __name__ == '__main__':
