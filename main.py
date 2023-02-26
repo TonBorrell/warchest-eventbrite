f1cf42515bc4baf0d22a5556c055c6f14f908859
tonborrell <tonborrell@gmail.com>
2023-02-26 21:15:27 +0100

    add recruit and attack movements

--- main.py
+++ main.py
@@ -22,9 +22,12 @@ class Warchest:
         )
         self.wolf = Player(
             [ControlZone((0, 2), "control")],
-            {Cavalry: 3, Knight: 3},
+            {"cavalry": 3, "knight": 3},
             {"cavalry": Cavalry, "knight": Knight},
         )
+        self.crow.other_player = self.wolf
+        self.wolf.other_player = self.crow
+
         self.current_player = self.decide_first_player()
 
     def decide_first_player(self):
@@ -32,6 +35,7 @@ class Warchest:
 
     def play(self):
         while not self.is_ended():
+            # TODO: Check if initiative
             # Show board
             # Create Hand
             self.current_player.create_hand()
@@ -70,6 +74,10 @@ class Player:
         self.hand = []
         self.discard_pile = []
         self.control_token = 3
+        self.other_player = None
+
+    def set_other_player(self, other_player):
+        self.other_player = other_player
 
     def create_hand(self):
         bag_list = []
@@ -108,7 +116,7 @@ class Player:
             if unit.name == "control":
                 if unit.is_close(pos):
                     # Set piece in board
-                    unit_class = self.units_dict[unit.lower()(pos)]
+                    unit_class = self.units_dict[unit.lower()](pos)
                     self.units.append(unit_class)
                     self.hand.remove(unit)
 
@@ -142,10 +150,34 @@ class Player:
                     self.hand.remove(unit_to_discard)
 
     def recruit(self):
-        pass
+        # Select unit to recruit
+        unit_to_discard = read_unit()
+        # Remove from hand and put on discard pile
+        self.hand.remove(unit_to_discard)
+        self.discard_pile.append(unit_to_discard)
+        # Get one from bag
+        for unit in self.bag:
+            if unit_to_discard == unit.name:
+                self.bag[unit] -= 1
 
     def attack(self):
-        pass
+        # Position to attack from
+        pos_initial = read_position()
+        # Unit of same type in hand
+        unit_to_attack_with = read_unit()
+        self.check_if_unit_in_hand(unit_to_attack_with)
+        # TODO: Loop until unit in hand
+        # To position
+        pos_final = read_position()
+        # Check if attack is posible
+        for unit in self.hand:
+            if unit.name == unit_to_attack_with.lower():
+                if unit.attack(pos_final):
+                    # Remove from hand and add to pile
+                    self.hand.remove(unit_to_attack_with)
+                    self.discard_pile.append(unit_to_attack_with)
+                    # Remove from game attacked unit
+                    # TODO: Remove other player by checking if match position
 
     def initiative(self):
         # Use unit
